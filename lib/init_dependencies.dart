1ae4badac9077b09be2b78952f73ed47c24cbebe
kal-i <paul.maceres00@gmail.com>
2025-01-03 00:21:25 +0800

    Integration of dependency injection with get_it package

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,50 @@
+import 'package:blog_posting_app/features/auth/data/data_sources/remote/auth_remote_data_source.dart';
+import 'package:blog_posting_app/features/auth/data/data_sources/remote/auth_remote_data_source_impl.dart';
+import 'package:blog_posting_app/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:blog_posting_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_posting_app/features/auth/domain/usecases/sign_up.dart';
+import 'package:blog_posting_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'core/secrets/app_secrets.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+
+  serviceLocator.registerLazySingleton(
+    () => supabase.client,
+  );
+}
+
+void _initAuth() {
+  serviceLocator.registerFactory<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(
+      supabaseClient: serviceLocator(),
+    ),
+  );
+
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepositoryImpl(
+      authRemoteDataSource: serviceLocator(),
+    ),
+  );
+
+  serviceLocator.registerFactory<SignUp>(
+    () => SignUp(
+      authRepository: serviceLocator(),
+    ),
+  );
+
+  serviceLocator.registerLazySingleton<AuthBloc>(
+    () => AuthBloc(
+      signUp: serviceLocator(),
+    ),
+  );
+}
